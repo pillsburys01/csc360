68c2b00e0a8cb76eabd50b85075c181a0a58c223
telemak <pillsburys01@gmail.com>
2018-02-06 13:40:30 -0500

    further work on laptop

--- week5/Laptop/src/Laptop.java
+++ week5/Laptop/src/Laptop.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Laptop implements Comparable<Laptop> {
 
-    double cpu, screenSize, weight, batteryLife, laptopScore;
+    double cpu, screenSize, weight, batteryLife;
     int ram, hdd, price;
     boolean graphicsCard;
-
+    double laptopScore;
     static final double cpuMax = 3.0;
     static final double ramMax = 32;
     static final double hddMax = 2048;
@@ -26,25 +26,65 @@ public class Laptop implements Comparable<Laptop> {
         hdd = hddSpec;
         price = priceSpec;
         graphicsCard = graphicsCardSpec;
+        int graphicsRating = (graphicsCard) ? 1 : 2;
 
-        laptopScore = 0;
+        laptopScore = (2 * cpu/cpuMax) + (2 * ram / ramMax) + (hdd / hddMax) + graphicsRating
+        + (screenSize / screenSizeMax) + (weight / weightMax) + (batteryLife / batteryLifeMax)
+        + (price/priceMax);
 
+/*
+    laptopScore = (2 * cpu/cpuMax) + (2 * ram/ramMax) + (1 * hdd/hddMax) + (graphics) + (1 * screen/screenMax) +
+    (1 * weight/weightMax) + (1 * battery/batteryMax) + (1 * price/priceMax)
+*/
+    }
 
+public void randomLaptopGenerator(int numTops){
 
+    double cpuSpec, screenSizeSpec, weightSpec, batteryLifeSpec;
+    int ramSpec, hddSpec, priceSpec;
+    boolean graphicsCardSpec;
 
+    List<Laptop> topList = new ArrayList<Laptop>();
 
+    while(numTops > 0){
+        cpuSpec = (Math.random() * 2) + 1;
+        screenSizeSpec = (Math.random() * 5) + 12;
+        weightSpec = (Math.random() * 4) + 2;
+        batteryLifeSpec = (Math.random() * 6) + 3;
+        ramSpec = (int)(Math.random() * 28) + 4;
+        hddSpec = (int)(Math.random() * 1920) + 128;
+        priceSpec = (int)(Math.random() * 1500) + 500;
+        double gCard = Math.random() * 2;
+
+        if(gCard > 1) graphicsCardSpec = false;
+        else graphicsCardSpec = true;
+
+        Laptop randomTop = new Laptop( cpuSpec, screenSizeSpec, weightSpec,
+         batteryLifeSpec, ramSpec, hddSpec, priceSpec,
+        graphicsCardSpec);
+
+        topList.add(randomTop);
+
+        numTops--;
 
     }
 
+}
+
+@Override
+    public String toString(){
 
 
+        return "";
+}
 
 
-
-
+@Override
     public int compareTo(Laptop otherTop ){
 
-        return 0;
+        if (this.laptopScore > otherTop.laptopScore) return 1;
+        else if (this.laptopScore < otherTop.laptopScore) return -1;
+        else return 0;
     }
 
 }
