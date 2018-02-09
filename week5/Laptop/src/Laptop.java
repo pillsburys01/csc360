1c2e11008c26c4cdcf642e971898d6aedcd36822
telemak <pillsburys01@gmail.com>
2018-02-09 17:04:32 -0500

    Laptop portion of ass2 is ready for submission. Revisiting Octagon.

--- week5/Laptop/src/Laptop.java
+++ week5/Laptop/src/Laptop.java
@@ -1,20 +1,34 @@
+/*
+Stephen Pillsbury
+Assignment 2
+CSC 360
+
+Laptop.java creates a list of randomly generated laptops, which have ratings
+for their cpu, ram, hdd, screen, price, and battery life specs. Each laptop
+is also assigned a score based on these ratings, with higher ratings giving
+a higher score.
+
+This list is then converted to an array for sorting purposes, printed, sorted,
+then printed again in order from lowest to highest score.
+ */
+
+
 import java.util.*;
 
 public class Laptop implements Comparable<Laptop> {
+    private double cpu, screenSize, weight, batteryLife;
+    private int ram, hdd, price;
+    private boolean graphicsCard;
+    private double laptopScore;
+    private static final double cpuMax = 3.0;
+    private static final double ramMax = 32;
+    private static final double hddMax = 2048;
+    private static final double screenSizeMax = 17.0;
+    private static final double weightMax = 6;
+    private static final double batteryLifeMax = 9;
+    private static final double priceMax = 2000;
 
-    double cpu, screenSize, weight, batteryLife;
-    int ram, hdd, price;
-    boolean graphicsCard;
-    double laptopScore;
-    static final double cpuMax = 3.0;
-    static final double ramMax = 32;
-    static final double hddMax = 2048;
-    static final double screenSizeMax = 17.0;
-    static final double weightMax = 6;
-    static final double batteryLifeMax = 9;
-    static final double priceMax = 2000;
-
-    public Laptop(double cpuSpec, double screenSizeSpec, double weightSpec,
+    private Laptop(double cpuSpec, double screenSizeSpec, double weightSpec,
                   double batteryLifeSpec, int ramSpec, int hddSpec, int priceSpec,
                   boolean graphicsCardSpec){
 
@@ -26,19 +40,38 @@ public class Laptop implements Comparable<Laptop> {
         hdd = hddSpec;
         price = priceSpec;
         graphicsCard = graphicsCardSpec;
-        int graphicsRating = (graphicsCard) ? 1 : 2;
+        int graphicsRating = (graphicsCard) ? 1 : 0;
 
         laptopScore = (2 * cpu/cpuMax) + (2 * ram / ramMax) + (hdd / hddMax) + graphicsRating
         + (screenSize / screenSizeMax) + (weight / weightMax) + (batteryLife / batteryLifeMax)
         + (price/priceMax);
 
-/*
-    laptopScore = (2 * cpu/cpuMax) + (2 * ram/ramMax) + (1 * hdd/hddMax) + (graphics) + (1 * screen/screenMax) +
-    (1 * weight/weightMax) + (1 * battery/batteryMax) + (1 * price/priceMax)
-*/
     }
 
-public void randomLaptopGenerator(int numTops){
+
+    public static void main(String[] args){
+
+       List<Laptop> topList = Laptop.randomLaptopCreator(5);
+       Laptop[] topArray = new Laptop[topList.size()];
+       topArray = topList.toArray(topArray);
+
+       System.out.println("Printing initial laptop list:");
+
+       for(Laptop randomTop : topArray){
+            System.out.println(randomTop.toString());
+        }
+
+       Arrays.sort(topArray);
+
+       System.out.println("Printing sorted laptop list:");
+
+       for(Laptop randomTop : topArray){
+            System.out.println(randomTop.toString());
+        }
+
+    }
+
+private static List<Laptop> randomLaptopCreator(int numTops){
 
     double cpuSpec, screenSizeSpec, weightSpec, batteryLifeSpec;
     int ramSpec, hddSpec, priceSpec;
@@ -47,20 +80,18 @@ public void randomLaptopGenerator(int numTops){
     List<Laptop> topList = new ArrayList<Laptop>();
 
     while(numTops > 0){
-        cpuSpec = (Math.random() * 2) + 1;
-        screenSizeSpec = (Math.random() * 5) + 12;
-        weightSpec = (Math.random() * 4) + 2;
-        batteryLifeSpec = (Math.random() * 6) + 3;
+        cpuSpec = (Math.random() * 2) + 1;         //for readability, the constants being added should
+        screenSizeSpec = (Math.random() * 5) + 12; //probably have been final variables, huh?
+        weightSpec = (Math.random() * 4) + 2;      //In any case, they represent the minimum "sane"
+        batteryLifeSpec = (Math.random() * 6) + 3; //value for each of the laptop attributes.
         ramSpec = (int)(Math.random() * 28) + 4;
         hddSpec = (int)(Math.random() * 1920) + 128;
         priceSpec = (int)(Math.random() * 1500) + 500;
         double gCard = Math.random() * 2;
-
-        if(gCard > 1) graphicsCardSpec = false;
-        else graphicsCardSpec = true;
+        graphicsCardSpec = (gCard > 1);
 
         Laptop randomTop = new Laptop( cpuSpec, screenSizeSpec, weightSpec,
-         batteryLifeSpec, ramSpec, hddSpec, priceSpec,
+        batteryLifeSpec, ramSpec, hddSpec, priceSpec,
         graphicsCardSpec);
 
         topList.add(randomTop);
@@ -68,23 +99,26 @@ public void randomLaptopGenerator(int numTops){
         numTops--;
 
     }
+   return topList;
 
 }
 
 @Override
     public String toString(){
 
+        return("Laptop Score: " + laptopScore + " CPU: " + cpu + " RAM: " + ram +
+                " Hard Drive Capacity: " + hdd + " Has Graphics Card: " + graphicsCard + " Screen Size: " +
+                screenSize + " Weight(pounds): " + weight +
+                " Battery Life(hours): " + batteryLife + " Price: " + price);
 
-        return "";
 }
 
 
 @Override
     public int compareTo(Laptop otherTop ){
 
-        if (this.laptopScore > otherTop.laptopScore) return 1;
-        else if (this.laptopScore < otherTop.laptopScore) return -1;
-        else return 0;
+        return Double.compare(this.laptopScore, otherTop.laptopScore);
+
     }
 
 }
